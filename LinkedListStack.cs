a865d7ae5e3de0046824632f38d9992430102ab8
lingesh2836 <sharanraj982@gmail.com>
2022-08-23 21:53:23 +0530

    Stacks And Queue

---
+++ LinkedListStack.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksandQueues
+{
+    class LinkedListStack
+    {
+        public Node top;
+
+        public LinkedListStack()
+        {
+            top = null;
+        }
+        public void push(int value)
+        {
+            Node node = new Node(value);
+            if (top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = top;
+            }
+            top = node;
+
+        }
+        internal void peek()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is Empty...");
+            }
+            Console.WriteLine("\nTop Most Element in Stack : " + top.data);
+        }
+
+        internal void pop()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is Empty... ");
+            }
+            Console.WriteLine("Deleted Element : " + top.data);
+            top = top.next;
+        }
+        internal void isEmpty()
+        {
+            while (top != null)
+            {
+                peek();
+                pop();
+            }
+            Console.WriteLine("Stack is Empty...");
+        }
+
+        public void Display()
+        {
+            Node temp = top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " -> ");
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace StacksandQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To LinkedListStackandQueues");
+            LinkedListStack stack = new LinkedListStack();
+            stack.push(70);
+            stack.push(30);
+            stack.push(56);
+            stack.Display();
+            stack.peek();
+            stack.pop();
+            stack.Display();
+            stack.isEmpty();
+            LinkedListQueue queue = new LinkedListQueue();
+            Console.WriteLine("\nLinkedList_Queue");
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.Display();
+            Console.WriteLine("Dequeue Element from Queue");
+            queue.Dequeue();
+            queue.Display();
+            Console.ReadLine();
+        }
+    }
+}
